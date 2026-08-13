40843ab2df4f44a958b16bf4b36ab883290ec911
田中透 <nigg@tanakatoorunoMacBook-Air.local>
2026-08-13 14:10:32 +0900

    fix: mapperの登録方法を修正

--- src/main/java/jp/co/hjn/merryapi/MerryApiApplication.java
+++ src/main/java/jp/co/hjn/merryapi/MerryApiApplication.java
@@ -1,11 +1,9 @@
 package jp.co.hjn.merryapi;
 
 import org.springframework.boot.SpringApplication;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("jp.co.hjn.merryapi.infrastructure.mapper")
 public class MerryApiApplication {
 
     public static void main(String[] args) {
